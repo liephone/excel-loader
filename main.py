61148518042a779c39432d1fdee2f315027bf267
liephone <kimkisun@gmail.com>
2026-05-03 15:39:08 +0900

    add middleware settings

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 # fastapi dev main.py
 from fastapi import FastAPI
 from api.excel_highlighter import router as excel_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     servers=[
         {"url": "https://excel-loader.onrender.com", "description": "Render Service server"},
         {"url": "http://127.0.0.1:8000", "description": "Local server"}
     ]
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # 모든 도메인 허용 (테스트용)
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # prefix를 "/excel"로 주면, excel.py의 모든 경로 앞에 /excel이 자동으로 붙습니다.
